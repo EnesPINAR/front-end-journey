d5f2dd9b122870fde1af8f8de794338cbab05c57
EnesPINAR <63356265+EnesPINAR@users.noreply.github.com>
2024-07-30 17:16:30 +0300

    recovering from backup
    manually uploaded from backup

--- mortgage-calculator/src/App.tsx
+++ mortgage-calculator/src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 //components
 import ClearForm from './components/ClearForm';
 import Input from './components/Input';
+import ResultArea from './components/ResultArea';
 
 function App() {
+
+  //states
+  const [amount, setAmount] = useState(0);
+  const [term, setTerm] = useState(0);
+  const [interestRate, setInterestRate] = useState(0);
+  const [monthly, setMonthly] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  //callbacks
+  const MortgageAmount = useCallback((childData) => {
+    setAmount(childData);
+  }, []);
+
+  const MortgageTerm = useCallback((childData) => {
+    setTerm(childData);
+  }, []);
+
+  const InterestRate = useCallback((childData) => {
+    setInterestRate(childData);
+  }, []);
 
   return (
     <>
@@ -16,21 +38,19 @@ function App() {
               <h2>Mortgage Calculator</h2>
               <ClearForm></ClearForm>
             </div>
-            <Input inputName="Mortgage Amount" icon="euro" iconDirection="start"></Input>
+            <Input inputName="Mortgage Amount" icon="euro" iconDirection="start" onMortgageAmount={MortgageAmount}></Input>
             <div className='termAndInterest'>
-              <Input inputName="Mortgage Term" icon="years" iconDirection="end"></Input>
-              <Input inputName="Interest Rate" icon="percent" iconDirection="end"></Input>
+              <Input inputName="Mortgage Term" icon="years" iconDirection="end" onMortgageTerm={MortgageTerm}></Input>
+              <Input inputName="Interest Rate" icon="percent" iconDirection="end" onInterestRate={InterestRate}></Input>
             </div>
-            <button type="submit">
+            <button type="submit" onClick={(e) => { e.preventDefault(); setTotal(amount * Math.pow(1 + interestRate / 100, term)); setMonthly(total / (term * 12)); setShowResult(true) }}>
               <img src="../src/assets/images/icon-calculator.svg" alt="calculator img" />
               Calculate Repayments
             </button>
           </form>
         </div>
         <div className="right">
-          <img src="../src/assets/images/illustration-empty.svg" alt="calculator img" />
-          <h3>Results shown here</h3>
-          <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+          <ResultArea showResult={showResult} monthly={monthly} total={total}></ResultArea>
         </div>
       </div>
     </>
---
+++ mortgage-calculator/src/components/ResultArea.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ResultArea = (props) => {
+    if (props.showResult) {
+        return <div className="result">
+            <h3>Your Results</h3>
+            <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click 'calculate repayments' again.</p>
+            <div className="ResultContainer">
+                <p>Your monthly repayments</p>
+                <p className="monthlyResult">€{props.monthly}</p>
+                <div className="line"></div>
+                <p>Total you will repay over the therm</p>
+                <p className="totalResult">€{props.total}</p>
+            </div>
+        </div>
+    } else {
+        return <div>
+            <img src="../src/assets/images/illustration-empty.svg" alt="calculator img" />
+            <h3>Results shown here</h3>
+            <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+        </div>
+    }
+}
+
+export default ResultArea;
